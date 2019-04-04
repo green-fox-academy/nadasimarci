5041ee1ae4c72dcddaf92bcb77ceec95e699a12d
Marcell NT <marcell@nadasitoth.com>
2019-04-04 15:44:31 +0200

    Basic info defined as variables

---
+++ week-01/day-4/define-basic-info.ts
@@ -0,0 +1,19 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as a number
+// Your height in meters as a number
+// Whether you are married or not as a boolean
+
+let myName: string = "Marci";
+console.log(myName)
+
+let myAge: number = 24;
+console.log(myAge)
+
+let myHeight: number = 1.74;
+console.log(myHeight)
+
+let iAmMarried: boolean = false;
+console.log(iAmMarried)
