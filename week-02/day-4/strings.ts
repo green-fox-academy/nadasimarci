b866ae511cbc3b34cf7ccbdea8e2035ee846ed7e
Marcell NT <marcell@nadasitoth.com>
2019-04-11 15:42:35 +0200

    feat(strings.ts): add 'use strict'

--- week-02/day-4/strings.ts
+++ week-02/day-4/strings.ts
@@ -1,6 +1,8 @@
 /* Given a string, compute recursively (no loops) a new string 
 where all the lowercase 'x' chars have been changed to 'y' chars.*/
 
+'use strict';
+
 function changeXtoY(str: string): string {
 
     if (str.indexOf('x') == -1) {
