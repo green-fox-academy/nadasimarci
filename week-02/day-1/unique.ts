12a0878396bb04adb229ce6ee8df600b842009dd
Marcell NT <marcell@nadasitoth.com>
2019-04-20 11:35:18 +0200

    feat(unique.ts): add new simpler solution

--- week-02/day-1/unique.ts
+++ week-02/day-1/unique.ts
@@ -4,22 +4,13 @@
 //  Returns a list of numbers where every number in the list occurs only once
 
 //  Example
-console.log(unique([1, 11, 34, 11, 52, 61, 1, 34]))
+// console.log(unique([1, 11, 34, 11, 52, 61, 1, 34]))
 //  should print: `[1, 11, 34, 52, 61]`
 
-function unique(arr: number[]) {
-  let uniqueArray: number[] = arr;
-  let index: number = 0;
-  let uniqueInd: number = 0;
-  //increment arr[]
-  for (index; index < arr.length; index++) {
-      //increment uniarr[]
-      for (uniqueInd; uniqueInd < arr.length; ++uniqueInd) {
-          if (uniqueArray[uniqueInd] === arr[index]) {
-              uniqueArray.splice(uniqueInd,1);
-          }
-      }       
-  }
-  //now it would return uniques only, but in as-is order
-  return uniqueArray.sort(function(a,b){return a-b});
+let myArray: number[] = [1, 11, 34, 11, 52, 61, 1, 34];
+
+function unique(arr: number[]): number[] {
+    return arr.filter((element, i) => arr.indexOf(element) === i);
 }
+
+console.log(unique(myArray));
