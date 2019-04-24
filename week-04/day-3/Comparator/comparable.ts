49ac4b3f5ed97f5f19a13c3d63a3b4e982f52db2
Marcell NT <marcell@nadasitoth.com>
2019-04-24 13:40:38 +0200

    chore(comparable.ts): create working file

---
+++ week-04/day-3/Comparator/comparable.ts
@@ -0,0 +1,12 @@
+'use strict';
+
+interface Comparable {
+  compareTo(other: Comparable): number;
+  values: number[];
+  /*
+  * returns negative number if this is smaller than other
+  * returns 0 if they are the same
+  * returns positive number if this is greater than other
+  */
+}
+
