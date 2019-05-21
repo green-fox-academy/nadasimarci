77f1931ea58aeac6f069a76839e13f853240fa7e
Marcell NT <marcell@nadasitoth.com>
2019-05-21 12:25:30 +0200

    chore(simple-timeout.js): copy working file and add callback function to print apple after 3 sec

---
+++ Orientation/week-07/day-2/simple-timeout.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Write a program that prints apple after 3 seconds.
+
+setTimeout(() => {
+  console.log('apple');
+}, 3000);
+  
+// repeat with the interval of 3 seconds
+// let timerId = setInterval(() => console.log('apple'), 3000);
