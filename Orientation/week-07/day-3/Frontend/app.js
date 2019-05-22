4536f5d3c904e96fa8db956fa02d3b38b9de9d10
Marcell NT <marcell@nadasitoth.com>
2019-05-22 11:59:00 +0200

    feat(app.js): add doubling

--- Orientation/week-07/day-3/Frontend/app.js
+++ Orientation/week-07/day-3/Frontend/app.js
@@ -10,6 +10,20 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/doubling', (req, res) => {
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  let response = {
+    received: req.query.input,
+    result: req.query.input * 2
+  }
+
+  if (req.query.input === undefined) {
+    res.end(JSON.stringify ({error: "Please provide an input!"}));
+  } else {
+    res.end(JSON.stringify(response));
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`The server is up and running on ${PORT}`);
 });
